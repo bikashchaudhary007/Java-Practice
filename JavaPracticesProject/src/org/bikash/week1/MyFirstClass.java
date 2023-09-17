bee4ed66ac3927df6985283ce5e21d99a39c209a
Bikash Chaudhary -2 <bikashchaudhary007@gmail.com>
2023-09-17 11:15:35 +0545

    inintal commit

---
+++ JavaPracticesProject/src/org/bikash/week1/MyFirstClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.bikash.week1;
+
+/**
+ * 
+ */
+public class MyFirstClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
